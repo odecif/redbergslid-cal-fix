03fe19dacbc12b6cfdea3676ff49fa5568fc5a59
Odecif <odecif@gmail.com>
2021-03-29 12:16:31 +0200

    Dynamisk hämtning av avdelningar
    * Istället för en fördefinerad lista över avdelningar hämtas nu istället
      kalender-sidan som traversas ner till en lista av dessa.
    
    * Hämtar även lista över andra kalendarar (kår, distrikt, riks). OBS!
      "Riks"-kalendern fungerar inte då scoutservice inte har samma
      kalenderplugin.
    
    * Introducerade BeautifulSoup för html-traverseringen.

--- main.py
+++ main.py
@@ -3,31 +3,48 @@ import requests
 from datetime import date, datetime, timedelta
 from icalendar import Calendar
 from io import BytesIO
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 app = Flask(__name__)
 
-avdelningar = [
-        ["Hela Kalendern", "0"],
-        ["Spårare", "sparare"],
-        ["Upptäckare", "upptackare"],
-        ["Äventyrare", "aventyrare"],
-        ["Utmanare", "utmanare"],
-        ["Rover", "rover"],
-        ["Funktionär", "ledare-funktionar"],
-        ["Styrelse", "styrelse"]
-    ]
+
+# Basfunktion för att hämta kalendersidan inför parsening
+def getSite(url="https://redbergslid.scout.se/kalender/lista/?"):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    return BeautifulSoup(html, "html.parser")
 
 
-def getAllCal(avdelning=0):
-    api = "https://redbergslid.scout.se/kalender/lista/?"
-    # tribe_event_category kan även vara distrikt/national
+# Hämta alla kalendertyper/avdelningar för kåren
+def getDepartmentsList():
+    soup = getSite()
+    avdelningar = []
+    for line in soup.find_all("span", {"class": "js-event-section"}):
+        avdelning = [line.string, line["data-slug"]]
+        avdelningar.append(avdelning)
+
+    return avdelningar
+
+
+# Hämta alla högra kalendar (kår, distrikt, nationell)
+def getCategories():
+    soup = getSite()
+    organlista = []
+    for line in soup.find_all("span", {"class": "js-event-category"}):
+        organ = [line.string, line["data-slug"]]
+        organlista.append(organ)
+
+    return organlista
+
+
+def getAllCal(avdelning=0, tribe_event_category="scout",
+              api="https://redbergslid.scout.se/kalender/lista/?"):
     parameters = {
-        "tribe_event_category": "scout",
-        "event-section": 0,
+        "tribe_event_category": tribe_event_category,
+        "event-section": avdelning,
         "tribe-bar-date": '1907-08-1',
         "ical": 0
     }
-    if avdelning != 0:
-        parameters["event-section"] = avdelning
     calendars = []
     currentdate = last_date = date.today()
     date1, date2 = currentdate, currentdate+timedelta(days=1)
@@ -54,25 +71,54 @@ def index():
     return_object = [
         """<body><header>
             <div class="container">
-                <h1 class="logo">Kalendrar</h1>
+                <h1 class="logo">Avdelningskalendrar</h1>
                 <p>Klicka för att ladda ned fil,
                 kopiera länkadress för att prenumerera på kalendern</p>
                 <strong><nav>
                     <ul class="menu">
         """]
-    for avdelning in avdelningar:
+    for avdelning in getDepartmentsList():
         return_object.append(
-                ''.join(["<li><a href=\"", base, avdelning[1], "\">",
+                ''.join(["<li><a href=\"", base, "avd/", avdelning[1], "\">",
                         avdelning[0], "</a></li>"]))
 
-    return_object.append("</ul></nav></strong></div></headers></body>")
+    return_object.append("</ul></nav></strong></div>")
+
+    return_object.append(
+            ''.join("""<div class="container">
+                <h1 class="logo">Andra kalendrar</h1>
+                <p>OBS! Nationella kalendern fungerar ej!</p>
+                <strong><nav>
+                    <ul class="menu">
+            """))
+    for category in getCategories():
+        return_object.append(
+                ''.join(["<li><a href=\"", base, "cat/", category[1], "\">",
+                        category[0], "</a></li>"]))
+
+    return_object.append("</headers></body>")
 
     return ''.join(return_object)
 
 
-@app.route("/<avdelning>")
-def giveFile(avdelning):
+@app.route("/avd/<avdelning>")
+def giveAvdFile(avdelning):
     calFile = BytesIO(getAllCal(avdelning))
+    calFile.seek(0)
+    return send_file(
+            calFile, attachment_filename='redbergslid.ics', as_attachment=True)
+
+
+@app.route("/cat/<category>")
+def giveCatFile(category):
+    api = "https://redbergslid.scout.se/kalender/lista/?"
+    if category == "district":
+        api = "https://www.gbgscout.se/kalender/lista/?"
+    # TODO: Nationella kalendern är i gamla formatet och kan inte exporteras
+    # på samma vis som de andra kalendrarna.
+    elif category == "national":
+        api = "http://scoutservice.se/kalendarium"
+    calFile = BytesIO(getAllCal(0, category, api))
     calFile.seek(0)
     return send_file(
             calFile, attachment_filename='redbergslid.ics', as_attachment=True)
